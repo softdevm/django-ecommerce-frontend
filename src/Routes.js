a6671c8695c1e20e8de74647ace889ff7ebee0fc
softdevm <michaelweaver275@gmail.com>
2021-03-13 15:54:36 +0530

    Implemented functionality for Private Routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './core/Home';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import PrivateRoutes from "./auth/helper/PrivateRoutes";
+import Home from "./core/Home";
 
 const Routes = () => {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={Home} />
-            </Switch>
-        </BrowserRouter>
-    );
-}
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        {/* <PrivateRoutes path="/user/dashboard" exact component={} /> */}
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default Routes;
